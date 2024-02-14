afbc4b865a6e0ef3b0e9b729e88d9e11cd796e6f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-14 11:49:39 +0900

    [Feat]: SecurityConfig deprecated 수정

--- src/main/java/com/group/libraryapp/core/config/SecurityConfig.java
+++ src/main/java/com/group/libraryapp/core/config/SecurityConfig.java
@@ -6,19 +6,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll() // 모든 요청에 대해 인증을 필요로 하지 않음
+                .and()
                 .csrf().disable()
-                .formLogin().disable()
-                .headers().frameOptions().disable();
+                .formLogin().disable();
+
+        return http.build();
     }
 }
