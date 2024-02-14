c013eed16ca8c61299fb428d57cdbbfefb741187
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-02-14 11:38:52 +0900

    [Feat]: RestControllerAdvice 로그 추가

--- src/main/java/com/group/libraryapp/presentation/error/ApiExceptionControllerAdvice.java
+++ src/main/java/com/group/libraryapp/presentation/error/ApiExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.group.libraryapp.presentation.error;
 
 import com.group.libraryapp.core.exception.ResourceNotFoundException;
 import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -10,17 +11,20 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionControllerAdvice {
 
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> noSuchElementExceptionHandler(NoSuchElementException e) {
+    public ResponseEntity<ErrorResponse> noSuchElementExceptionHandler(HttpServletRequest request, NoSuchElementException e) {
+        log.info("NoSuchElementException {}", e);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .error("NoSuchElementException")
                 .message(e.getMessage())
@@ -29,13 +33,14 @@ public class ApiExceptionControllerAdvice {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e){
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException e){
         BindingResult bindingResult = e.getBindingResult();
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 
         Map<String, String> errors = new HashMap<>();
         for (FieldError fieldError : fieldErrors)
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        log.info("MethodArgumentNotValidException {}", errors);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .error("MethodArgumentNotValidException")
                 .message(errors.toString())
@@ -43,7 +48,8 @@ public class ApiExceptionControllerAdvice {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+    public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(HttpServletRequest request, IllegalArgumentException e) {
+        log.info("IllegalArgumentException {}", e);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .error("IllegalArgumentException")
                 .message(e.getMessage())
@@ -52,7 +58,8 @@ public class ApiExceptionControllerAdvice {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> jwtExceptionHandler(JwtException e) {
+    public ResponseEntity<ErrorResponse> jwtExceptionHandler(HttpServletRequest request, JwtException e) {
+        log.info("JwtException {}", e);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .error("JwtException")
                 .message(e.getMessage())
@@ -61,7 +68,9 @@ public class ApiExceptionControllerAdvice {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> exceptionHandler(ResourceNotFoundException e) {
+    public ResponseEntity<ErrorResponse> exceptionHandler(HttpServletRequest request, ResourceNotFoundException e) {
+        var rawToken = request.getHeader("Authorization");
+        log.warn("ResourceNotFoundException {}, token = {}", e, rawToken);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .error("Exception")
                 .message(e.getMessage())
